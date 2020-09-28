6b0c6bd5e35b75f71e63d56b3bd741fa75f5ba48
Glitch (plucky-whimsical-jaxartosaurus) <none>
2020-09-28 15:08:56 +0000

    📙🦀 Updated with Glitch

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const app = express();
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + "/index.html");
 });
 
 
